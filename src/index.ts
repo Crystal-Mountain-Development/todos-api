9848fc85c7da5d51cfda8820ed018d26b3abcae1
esteban <luigirp64@gmail.com>
2020-12-12 12:33:15 -0600

    implement apollo server [TL-8]

--- src/index.ts
+++ src/index.ts
@@ -6,51 +6,34 @@ import { List } from "./entity/List";
 import { Todo } from "./entity/Todo";
 import { User } from "./entity/User";
 
+import { ApolloServer,gql } from "apollo-server"
+
+const typeDefs = gql`
+
+  type User {
+    id: String!
+    username: String!
+    email: String!
+  }
+
+  type Query {
+    users: [User!]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users: () => User.find()
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+
 createConnection()
   .then(async () => {
-    console.log("Inserting a new user into the database...");
-
-    const user = new User();
-    user.email = "user@example.com";
-    user.username = "user";
-    await user.save();
-
-    console.log("Saved a new user with id: " + user.id);
-
-    console.log("Loading users from the database...");
-    const users = await User.find();
-    console.log("Loaded users: ", users);
-
-    const authToken = new AuthToken();
-    authToken.user = user;
-    authToken.val = "4865";
-    authToken.expDate = dayjs().add(30, "minute").toDate();
-
-    await authToken.save();
-    console.log(
-      "New Authentication Token: " +
-        authToken.val +
-        ": " +
-        authToken.expDate +
-        ": " +
-        authToken.id
-    );
-
-    const tokens = await AuthToken.find();
-    console.log("Tokens: ", tokens);
-
-    const list = new List();
-    list.user = user;
-    list.title = "dummy";
-    list.isComplete = false;
-    await list.save();
-
-    const todos = new Todo();
-    todos.summary = "dummy with honey";
-    todos.isComplete = false;
-    todos.list = list;
-    await todos.save();
-
-    console.log("Here you can setup and run express/koa/any other framework.");
+    server.listen().then(({ url }) => {
+      console.log(`🚀  Server ready at ${url}`);
+    });
   })
   .catch((error) => console.log(error));
