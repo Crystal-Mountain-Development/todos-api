ac365431c216512f06fb638ec1449e35ab841c5c
Esteban <Luigirp64@gmail.com>
2021-02-03 09:38:09 -0600

    Fix username not to be used by another user [TL-16]

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -2,6 +2,8 @@ export const LOGIN_REQUIRED = "You must login";
 
 export const ALREADY_ACCOUNT = "Already have a account";
 
+export const ALREADY_NAME = "User name already take";
+
 export const INVALID_EMAIL = "Invalid Email";
 
 export const INVALID_TOKEN = "Invalid Token";
